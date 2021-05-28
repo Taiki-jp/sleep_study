70f82cb684fbade6d93d4f68954553fe4d91b96e
taiki-jp <taiki-dosisha@outlook.jp>
2021-05-28 13:36:25 +0900

    feat(tmp):tmp commit

--- custom_loop_main.py
+++ custom_loop_main.py
@@ -25,13 +25,13 @@ except:
 # float32が推奨されているみたい
 tf.keras.backend.set_floatx('float32')
 # tf.functionのせいでデバッグがしずらい問題を解決してくれる（これを使わないことでエラーが起こらなかったりする）
-tf.config.run_functions_eagerly(True)
+#tf.config.run_functions_eagerly(True)
 # ================================================ #
 #  *                メイン関数
 # ================================================ #
 
 def main(name, project, train, test, 
-         epoch=1, isSaveModel=False, my_tags=None, mul_num=False, 
+         epochs=1, isSaveModel=False, my_tags=None, mul_num=False, 
          checkpoint_path=None, is_attention=True, my_confusion_file_name=None,
          id = None, batch_size=32, n_class=5):
     
@@ -52,85 +52,67 @@ def main(name, project, train, test,
     y_train-=1
     y_test-=1
     # convert2one-hot
-    y_train = tf.one_hot(y_train, n_class)
-    y_test = tf.one_hot(y_test, n_class)
+    #y_train = tf.one_hot(y_train, n_class)
+    #y_test = tf.one_hot(y_test, n_class)
     # change x shape
     x_train = x_train[:,:,:,np.newaxis]
     x_test = x_test[:,:,:,np.newaxis]
     train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-    
-    
-    # ================================================ #
-    #  *             データ保存先の設定
-    # ================================================ #
-    # TODO : ほかのファイルでこれできないの？
-    ss_list_for_wandb = ["NR34", "NR2", "NR1", "REM", "WAKE"]
-    wandb.init(name = name, 
-               project = project,
-               tags = my_tags,
-               config= \
-               {
-                   "test id":m_preProcess.test_data_for_wandb,
-                    "train num":x_train.shape[0],
-                    "test num":x_test.shape[0],
-                    "date id":id,
-                    "test wake before replaced":ss_test_dict[5],
-                    "test rem before replaced":ss_test_dict[4],
-                    "test nr1 before replaced":ss_test_dict[3],
-                    "test nr2 before replaced":ss_test_dict[2],
-                    "test nr34 before replaced":ss_test_dict[1],
-                    "train wake before replaced":ss_train_dict[5],
-                    "train rem before replaced":ss_train_dict[4],
-                    "train nr1 before replaced":ss_train_dict[3],
-                    "train nr2 before replaced":ss_train_dict[2],
-                    "train nr34 before replaced":ss_train_dict[1]
-                })
-    
-    # ================================================ #
-    #*         モデル作成（ネットから取ってくる方）
-    # ================================================ #
+    #train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+    val_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+    #val_dataset = val_dataset.batch(batch_size)
     
     inputs = tf.keras.Input(shape=(128, 512, 1))
     outputs = edl_classifier_2d(x=inputs, n_class=n_class)
-    model = EDLModelBase(inputs=inputs, outputs=outputs)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
     
-    # ================================================ #
-    #*       モデルのコンパイル（サブクラスなし）
-    # ================================================ #
+    optimizer = tf.keras.optimizers.Adam()
+    # TODO : どのような形で引数に渡すべきかmnistの例をもとに調査する
+    # true side : カテゴリカルな状態，pred side : クラスの次元数（ソフトマックスをかける前）
+    train_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
+    val_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
     
-    model.compile(optimizer=tf.keras.optimizers.Adam(),
-                  loss=EDLLoss(K=n_class),
-                  metrics=["accuracy"])
-    
-    # ================================================ #
-    #*                   モデル学習
-    # ================================================ #
-    w_callBack = WandbClassificationCallback(validation_data=(x_test, y_test),
-                                             training_data=(x_train, y_train),
-                                             log_confusion_matrix=False,
-                                             labels=["nr34", "nr2", "nr1", "rem", "wake"],
-                                             show_my_confusion_title=True,
-                                             my_confusion_title="confusion matrix",
-                                             my_confusion_file_name=my_confusion_file_name,
-                                             log_f_measure=False,calc_metrics=False)
-    
-    # NOTE : モデルの各チェックポイントでの保存部分は削除
-    if checkpoint_path:
-        pass
-        # tf_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=False, verbose=3)
-    
-    model.fit(x_train,
-              y_train,
-              batch_size=batch_size,
-              validation_data = (x_test, y_test),
-              epochs = epoch,
-              callbacks = [w_callBack],
-              verbose = 2)
-    
-    if isSaveModel:
-        path = os.path.join(os.environ["sleep"], "models", id)
-        model.save(path)
-    wandb.finish()
+    # エポックのループ
+    for epoch in range(epochs):
+        # ロス関数はエポックごとにアニーリングを変えるので中に書く
+        loss_fn = EDLLoss(K=n_class, annealing=epoch/epochs)
+        print(f"エポック:{epoch} スタート")
+        # エポック内のバッチサイズごとのループ
+        for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+            # 勾配を計算
+            with tf.GradientTape() as tape:
+                # NOTE : x_batch_trainの次元は3である
+                #assert np.ndim(x_batch_train) == 3
+                evidence = model(tf.expand_dims(x_batch_train, axis=0), training=True)
+                alpha = evidence+1
+                y_pred = alpha/tf.reduce_sum(alpha, axis=1, keepdims=True)
+                loss_value = loss_fn.call(y_batch_train, alpha)  # NOTE : one-hotの形で入れるべき？
+            
+            grads = tape.gradient(loss_value, model.trainable_weights)
+            optimizer.apply_gradients(zip(grads, model.trainable_weights))
+            
+            train_acc_metric.update_state(y_batch_train, y_pred)
+            
+            #if step % 200 == 0:
+            #    print(f"step:{step}, loss:{float(loss_value)}")
+        
+        # エポックの終わりにメトリクスを表示する
+        train_acc = train_acc_metric.result()
+        print(f"エポック中の訓練一致率：{train_acc}")
+        # エポックの終わりに訓練メトリクスを初期化
+        train_acc_metric.reset_states()
+        
+        # 訓練の終わりに検証用データ（今回はテストデータ）の性能を見る
+        for x_batch_val, y_batch_val in val_dataset:
+            val_evidence = model(tf.expand_dims(x_batch_val, axis=0), training=False)
+            val_alpha = val_evidence+1
+            val_y_pred = val_alpha/tf.reduce_sum(val_alpha, axis=1, keepdims=True)
+            val_acc_metric.update_state(y_batch_val, val_y_pred)  # NOTE : one-hotの形で入れるべき？
+        # メトリクスを表示
+        val_acc = val_acc_metric.result()
+        print(f"検証用データの一致率：{val_acc}")
+        # 初期化
+        val_acc_metric.reset_states()
 
 # ================================================ #
 #  *                ループ処理
@@ -156,7 +138,7 @@ if __name__ == '__main__':
         m_preProcess.check_path_auto(cm_file_name)
     
         main(name = "edl", project = "edl",
-             train=train, test=test, epoch=100, isSaveModel=True, mul_num=MUL_NUM,
+             train=train, test=test, epochs=100, isSaveModel=True, mul_num=MUL_NUM,
              my_tags=["loss_all", f"{list(Utils().name_dict.keys())[test_id]}", f"train:1:{MUL_NUM}", attention_tag],
              checkpoint_path=None, is_attention = is_attention, 
              my_confusion_file_name=cm_file_name, id=id)
