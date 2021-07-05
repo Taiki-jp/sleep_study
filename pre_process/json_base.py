5d51fa90dcbe3126ea8cb4451556f4ed85788ce0
taiki-jp <taiki-dosisha@outlook.jp>
2021-07-06 00:09:18 +0900

    tmp commit

---
+++ pre_process/json_base.py
@@ -0,0 +1,35 @@
+import sys, os, glob, json
+from dataclasses import dataclass
+
+@dataclass
+class JsonBase(object):
+    json_file : str = os.path.join(os.environ["userprofile"],
+                                   "git",
+                                   "sleep_study",
+                                   "pre_process",
+                                   json_name)
+    json_dict : dict = {}
+    prev_names : list = []
+    prev_sass : list = []
+    foll_names : list = []
+    foll_sass : list = []
+    
+    def load(self) -> None:
+        with open(self.json_file) as f:
+            self.json_dict = json.load(f)
+        self.prev_names = self.json_dict["prev_subjects_name"]
+        self.prev_sass = self.json_dict["prev_sas_name"]
+        self.foll_names = self.json_dict["following_subjects_name"]
+        self.foll_sass = self.json_dict["following_sas_name"]
+    
+if __name__ == '__main__':
+    from data_analysis.py_color import PyColor
+    sl = SubjectsList()
+    sl.load()
+    for key, val in sl.__dict__.items():
+        print(PyColor.GREEN,
+            "key, ", key, 
+            "val, ", val, 
+            PyColor.END)
+
+    
