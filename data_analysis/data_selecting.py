0b5983786d14c1f8d15087a8024f632dd99808bc
taiki-jp <taiki-dosisha@outlook.jp>
2021-11-09 15:13:31 +0900

    feat: for spectrogram

--- data_analysis/data_selecting.py
+++ data_analysis/data_selecting.py
@@ -11,7 +11,7 @@ from data_analysis.py_color import PyColor
 from data_analysis.utils import Utils
 from mywandb.utils import make_ss_dict4wandb
 from nn.losses import EDLLoss
-from nn.model_base import EDLModelBase, edl_classifier_1d
+from nn.model_base import EDLModelBase, edl_classifier_1d, edl_classifier_2d
 from nn.utils import load_model, separate_unc_data
 from nn.wandb_classification_callback import WandbClassificationCallback
 from pre_process.json_base import JsonBase
@@ -45,6 +45,7 @@ def main(
     experiment_type: str = "",
     saving_date_id: str = "",
     has_dropout: bool = False,
+    dropout_rate: float = 0,
 ):
 
     # データセットの作成
@@ -90,23 +91,35 @@ def main(
 
     # NOTE: earernel_size の半分が入力のサイズになる（fft をかけているため）
     # TODO: mypyでshapeが違うとエラーになる
-    if data_type == "spectrum":
+    if data_type == "spectrum" or data_type == "cepstrum":
         shape: Tuple[int, int] = (int(kernel_size / 2), 1)
     elif data_type == "spectrogram":
-        shape: Tuple[int, int, int] = (128, 512, 1)
+        shape: Tuple[int, int, int] = (128, 30, 1)
     else:
-        print("correct here based on your model")
+        print("correct data_type based on your model")
         sys.exit(1)
 
     inputs = tf.keras.Input(shape=shape)
-    outputs = edl_classifier_1d(
-        x=inputs,
-        n_class=n_class,
-        has_attention=has_attention,
-        has_inception=has_inception,
-        is_mul_layer=is_mul_layer,
-        has_dropout=has_dropout,
-    )
+    if data_type == "spectrum" or data_type == "cepstrum":
+        outputs = edl_classifier_1d(
+            x=inputs,
+            n_class=n_class,
+            has_attention=has_attention,
+            has_inception=has_inception,
+            is_mul_layer=is_mul_layer,
+            has_dropout=has_dropout,
+            dropout_rate=dropout_rate,
+        )
+    elif data_type == "spectrogram":
+        outputs = edl_classifier_2d(
+            x=inputs,
+            n_class=n_class,
+            has_attention=has_attention,
+            has_inception=has_inception,
+            is_mul_layer=is_mul_layer,
+            has_dropout=has_dropout,
+            dropout_rate=dropout_rate,
+        )
 
     model = load_model(
         loaded_name=test_name, model_id=date_id, n_class=n_class, verbose=0
@@ -227,23 +240,23 @@ if __name__ == "__main__":
     # ハイパーパラメータの設定
     # TODO: jsonに移植
     TEST_RUN = False
+    EPOCHS = 50
     HAS_ATTENTION = True
     PSE_DATA = False
     HAS_INCEPTION = True
-    IS_PREVIOUS = False
+    IS_PREVIOUS = True
     IS_NORMAL = True
     IS_ENN = True  # FIXME: always true so remove here
     IS_MUL_LAYER = False
     CATCH_NREM2 = True
     HAS_DROPOUT = True
-    EPOCHS = 100
-    BATCH_SIZE = 128
+    BATCH_SIZE = 64
     N_CLASS = 5
-    KERNEL_SIZE = 512
-    STRIDE = 480
-    SAMPLE_SIZE = 10000
-    UNC_THRETHOLD = 0.5
-    DATA_TYPE = "spectrum"
+    KERNEL_SIZE = 256
+    STRIDE = 16
+    SAMPLE_SIZE = 2000
+    UNC_THRETHOLD = 0.3
+    DATA_TYPE = "spectrogram"
     FIT_POS = "middle"
     EXPERIMENT_TYPES = (
         "no_cleansing",
@@ -277,10 +290,15 @@ if __name__ == "__main__":
     utils = Utils(catch_nrem2=CATCH_NREM2)
 
     # 読み込むモデルの日付リストを返す
-    JB = JsonBase("../nn/model_id.json")
+    JB = JsonBase("model_id.json")
     JB.load()
     model_date_list = JB.make_list_of_dict_from_mul_list(
-        "enn", "spectrum", "middle", "stride_480", "kernel_512"
+        "normal_prev",
+        "enn",
+        DATA_TYPE,
+        "middle",
+        f"stride_{STRIDE}",
+        f"kernel_{KERNEL_SIZE}",
     )
     try:
         assert len(model_date_list) != 0
@@ -292,7 +310,7 @@ if __name__ == "__main__":
     subjects_id = [i for i in range(len(pre_process.name_list))]
 
     for test_id, test_name, date_id in zip(
-        subjects_id[47:], pre_process.name_list[47:], model_date_list[47:]
+        subjects_id, pre_process.name_list, model_date_list
     ):
         (train, test) = pre_process.split_train_test_from_records(
             datasets, test_id=test_id, pse_data=PSE_DATA
@@ -361,6 +379,9 @@ if __name__ == "__main__":
     # TODO: モデルの保存を行う変数に応じて場合分け
     JB.dump(
         keys=[
+            JB.first_key_of_pre_process(
+                is_normal=IS_NORMAL, is_prev=IS_PREVIOUS
+            ),
             ENN_TAG,
             DATA_TYPE,
             FIT_POS,
