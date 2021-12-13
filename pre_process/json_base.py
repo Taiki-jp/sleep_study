6eea5377c796cbd49a3ee75b4c764fbc6826fa1b
taiki-jp <taiki-dosisha@outlook.jp>
2021-12-13 09:04:02 +0900

    fix: json base class

--- pre_process/json_base.py
+++ pre_process/json_base.py
@@ -1,5 +1,6 @@
 import json
 import os
+import socket
 import sys
 
 from data_analysis.py_color import PyColor
@@ -7,6 +8,7 @@ from data_analysis.py_color import PyColor
 
 class JsonBase(object):
     def __init__(self, json_filename: str) -> None:
+        self.hostname = socket.gethostname()
         self.json_file: str = os.path.join(
             os.environ["git"],
             "sleep_study",
@@ -18,99 +20,20 @@ class JsonBase(object):
         self.prev_sass: list = []
         self.foll_names: list = []
         self.foll_sass: list = []
-
-    def load(self) -> None:
-        with open(self.json_file) as f:
-            self.json_dict = json.load(f)
-
-    def dump(self, keys: list, value: str, is_pre_dump: bool = False) -> None:
-        if is_pre_dump:
-            print(PyColor().RED_FLASH, "jsonに出力できるか確認します", PyColor().END)
-        # 辞書であり，keysが入っている間はキーを入れる
-        key_len = len(keys)
-        if key_len > 7:
-            print(PyColor().RED_FLASH, "キーは7までの長さまでしか実装されていません", PyColor().END)
-            sys.exit(1)
-        # TODO : もっと賢い書き方無いかな？
-        if key_len == 1:
-            self.json_dict[keys[0]] = value
-        elif key_len == 2:
-            self.json_dict[keys[0]][keys[1]] = value
-        elif key_len == 3:
-            self.json_dict[keys[0]][keys[1]][keys[2]] = value
-        elif key_len == 4:
-            self.json_dict[keys[0]][keys[1]][keys[2]][keys[3]] = value
-        elif key_len == 5:
-            self.json_dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]] = value
-        elif key_len == 6:
-            self.json_dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][
-                keys[5]
-            ] = value
-        elif key_len == 7:
-            self.json_dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][
-                keys[5]
-            ][keys[6]] = value
-        with open(self.json_file, "w") as f:
-            json.dump(self.json_dict, f, indent=2)
-
-    # 複数リスト => 要素を辞書にまとめたリストを作成するメソッド
-    def make_list_of_dict_from_mul_list(self, *args) -> list:
-        first_list = self.json_dict[args[0]][args[1]][args[2]][args[3]][
-            args[4]
-        ][args[5]]["no_cleansing"]
-        second_list = self.json_dict[args[0]][args[1]][args[2]][args[3]][
-            args[4]
-        ][args[5]]["positive_cleansing"]
-        # third_list = self.json_dict[args[0]][args[1]][args[2]][args[3]][
-        #     args[4]
-        # ]["negative_cleansing"]
-
-        # マップ関数によって iterable に変換してる kigasuru
-        # TODO: 最大のものに合わせてループを繰り返すようにする
-        mapped = map(
-            lambda x, y, z: dict(nothing=x, positive=y, negative=z),
-            first_list,
-            second_list,
-            first_list,
-        )
-
-        return list(mapped)
-
-    # per_processed_id.jsonのフォーマット作成
-    def make_pre_processed_id_format(self) -> None:
-        # キーを指定して辞書を作成するために，同じ階層の辞書をとりあえず作る
-        os_version = ["win_249", "spica-2nd"]
-        dataset = ["normal_prev", "normal_follow", "sas_prev", "sas_follow"]
-        preprocess_type = ["spectrum", "spectrogram", "cepstrum"]
-        ss_pos = ["bottom", "middle", "top"]
-        stride = ["stride_" + str(i) for i in (1, 4, 16, 480, 1024)]
-        kernel = ["kernel_" + str(i) for i in (512, 1024)]
-
-        # 辞書の初期値のみをループとは別に作成しておく
-        kernel_d = {_kernel: "" for _kernel in kernel}
-        # リストの後ろにあるものほど、後にループ処理がかかるのでJsonの浅い場所のキーとなる
-        list4loop = [stride, ss_pos, preprocess_type, dataset, os_version]
-        for _list4loop in list4loop:
-            merged_d = {__list4loop: kernel_d for __list4loop in _list4loop}
-            kernel_d = merged_d.copy()
-        # jsonの中身の書き換え
-        self.json_dict = merged_d
-        # jsonfileへの書き込み
-        try:
-            with open(self.json_file, "w") as f:
-                json.dump(self.json_dict, f, indent=2)
-        except Exception:
-            print("jsonへの書き込みに失敗しました")
-            sys.exit(1)
-
-    # subjects_info.jsonのフォーマット作成
-    def make_subjects_info_format(self) -> None:
-        # 読み込んだjson形式のコピーをとる
-        json_prev = self.json_dict.copy()
-        # 読み込んだjson形式の末尾に追加する項目
-        added_key = ["birth", "sex", "sleeping_time"]
-        added_key_d = {_added_key: "" for _added_key in added_key}
-        subjects_list = [
+        # 各json形式で共通のキーはクラスメンバとして定義
+        self.os_version: list = ["win_249", "spica-2nd", "home-pc"]
+        self.dataset = [
+            "normal_prev",
+            "normal_follow",
+            "sas_prev",
+            "sas_follow",
+        ]
+        self.preprocess_type = ["spectrum", "spectrogram", "cepstrum"]
+        self.ss_pos = ["bottom", "middle", "top"]
+        self.stride = ["stride_" + str(i) for i in (1, 4, 16, 480, 1024)]
+        self.kernel = ["kernel_" + str(i) for i in (512, 1024)]
+        self.added_key = ["birth", "sex", "sleeping_time"]
+        self.subjects_list = [
             "H_Li",
             "H_Murakami",
             "H_Yamamoto",
@@ -242,8 +165,129 @@ class JsonBase(object):
             "141215_Tomura",
             "151125_Umenai",
         ]
+        self.model_type = ["dnn", "enn"]
+        self.cleansing_type = [
+            "no_cleansing",
+            "positive_cleansing",
+            "negative_cleansing",
+        ]
 
-        merged_d = {__subject: added_key_d for __subject in subjects_list}
+    def load(self) -> None:
+        with open(self.json_file) as f:
+            self.json_dict = json.load(f)
+
+    def dump(
+        self, keys: list = [], value: str = "", is_pre_dump: bool = False
+    ) -> None:
+        def __dump():
+            # 辞書であり，keysが入っている間はキーを入れる
+            key_len = len(keys)
+            if key_len > 8:
+                print(
+                    PyColor().RED_FLASH,
+                    "キーは7までの長さまでしか実装されていません",
+                    PyColor().END,
+                )
+                sys.exit(1)
+            # TODO : もっと賢い書き方無いかな？
+            if key_len == 1:
+                self.json_dict[keys[0]] = value
+            elif key_len == 2:
+                self.json_dict[keys[0]][keys[1]] = value
+            elif key_len == 3:
+                self.json_dict[keys[0]][keys[1]][keys[2]] = value
+            elif key_len == 4:
+                self.json_dict[keys[0]][keys[1]][keys[2]][keys[3]] = value
+            elif key_len == 5:
+                self.json_dict[keys[0]][keys[1]][keys[2]][keys[3]][
+                    keys[4]
+                ] = value
+            elif key_len == 6:
+                self.json_dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][
+                    keys[5]
+                ] = value
+            elif key_len == 7:
+                self.json_dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][
+                    keys[5]
+                ][keys[6]] = value
+            elif key_len == 8:
+                self.json_dict[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]][
+                    keys[5]
+                ][keys[6]][keys[7]] = value
+            with open(self.json_file, "w") as f:
+                json.dump(self.json_dict, f, indent=2)
+
+        if is_pre_dump:
+            print(
+                PyColor().RED_FLASH,
+                f"comfirming whether dump into {self.json_file} is possipoble or not",
+                PyColor().END,
+            )
+        else:
+            print(
+                PyColor().GREEN_FLASH,
+                f"dumping into {self.json_file}",
+                PyColor().END,
+            )
+
+        __dump()
+
+    # 複数リスト => 要素を辞書にまとめたリストを作成するメソッド
+    def make_list_of_dict_from_mul_list(self, *args) -> list:
+        first_list = self.json_dict[args[0]][args[1]][args[2]][args[3]][
+            args[4]
+        ][args[5]]["no_cleansing"]
+        second_list = self.json_dict[args[0]][args[1]][args[2]][args[3]][
+            args[4]
+        ][args[5]]["positive_cleansing"]
+        # third_list = self.json_dict[args[0]][args[1]][args[2]][args[3]][
+        #     args[4]
+        # ]["negative_cleansing"]
+
+        # マップ関数によって iterable に変換してる kigasuru
+        # TODO: 最大のものに合わせてループを繰り返すようにする
+        mapped = map(
+            lambda x, y, z: dict(nothing=x, positive=y, negative=z),
+            first_list,
+            second_list,
+            first_list,
+        )
+
+        return list(mapped)
+
+    def make_deep_dict(self, list4loop: list, first_dict: dict) -> None:
+        for _list4loop in list4loop:
+            merged_d = {__list4loop: first_dict for __list4loop in _list4loop}
+            first_dict = merged_d.copy()
+        # jsonの中身の書き換え
+        self.json_dict = merged_d
+        # jsonfileへの書き込み
+        try:
+            with open(self.json_file, "w") as f:
+                json.dump(self.json_dict, f, indent=2)
+        except Exception:
+            print("jsonへの書き込みに失敗しました")
+            sys.exit(1)
+
+    # per_processed_id.jsonのフォーマット作成
+    def make_pre_processed_id_format(self) -> None:
+        # 辞書の初期値のみをループとは別に作成しておく
+        kernel_d = {_kernel: "" for _kernel in self.kernel}
+        # リストの後ろにあるものほど、後にループ処理がかかるのでJsonの浅い場所のキーとなる
+        list4loop = [
+            self.stride,
+            self.ss_pos,
+            self.preprocess_type,
+            self.dataset,
+            self.os_version,
+        ]
+        self.make_deep_dict(list4loop, kernel_d)
+
+    # subjects_info.jsonのフォーマット作成
+    def make_subjects_info_format(self) -> None:
+        # 読み込んだjson形式の末尾に追加する項目
+        added_key_d = {_added_key: "" for _added_key in self.added_key}
+        merged_d = {__subject: added_key_d for __subject in self.subjects_list}
         # jsonの中身の書き換え
         self.json_dict = merged_d
         # jsonfileへの書き込み
@@ -255,107 +299,21 @@ class JsonBase(object):
             sys.exit(1)
 
     # model_id.jsonのフォーマット作成
-
     def make_model_id_format(self) -> None:
-        # キーを指定して辞書を作成するために，同じ階層の辞書をとりあえず作る
-        os_version = ["win_249", "spica-2nd"]
-        dataset = [
-            "normal_prev",
-            "normal_follow",
-            "sas_prev",
-            "sav_normal",
-            "unused_set",
-        ]
-        model_type = ["dnn", "enn"]
-        preprocess_type = ["spectrum", "spectrogram"]
-        ss_pos = ["bottom", "middle", "top"]
-        stride = ["stride_" + str(i) for i in (1, 4, 16, 480, 1024)]
-        kernel = ["kernel_" + str(i) for i in (256, 512, 1024)]
-        cleansing_type = [
-            "no_cleansing",
-            "positive_cleansing",
-            "negative_cleansing",
-        ]
-
         # 辞書の初期値のみをループとは別に作成しておく
         cleansing_type_d = {
-            _cleansing_type: [] for _cleansing_type in cleansing_type
+            _cleansing_type: [] for _cleansing_type in self.cleansing_type
         }
         list4loop = [
-            kernel,
-            stride,
-            ss_pos,
-            preprocess_type,
-            model_type,
-            dataset,
-            os_version,
+            self.kernel,
+            self.stride,
+            self.ss_pos,
+            self.preprocess_type,
+            self.model_type,
+            self.dataset,
+            self.os_version,
         ]
-        for _list4loop in list4loop:
-            merged_d = {
-                __list4loop: cleansing_type_d for __list4loop in _list4loop
-            }
-            cleansing_type_d = merged_d.copy()
-        # jsonの中身の書き換え
-        self.json_dict = merged_d
-        # jsonfileへの書き込み
-        try:
-            with open(self.json_file, "w") as f:
-                json.dump(self.json_dict, f, indent=2)
-        except Exception:
-            print("jsonへの書き込みに失敗しました")
-            sys.exit(1)
-
-    # main_param.jsonのフォーマット作成
-    # NOTE: 使わない
-    def make_main_param_format(self) -> None:
-        # キーを指定して辞書を作成するために，同じ階層の辞書をとりあえず作る
-        dataset = [
-            "main",
-            "main_custom",
-            "pre_process_main",
-            "data_analysis_main",
-            "data_merging",
-            "data_selecting",
-        ]
-        params = [
-            "data_type",
-            "fit_pos",
-            "stride",
-            "test_run",
-            "epochs",
-            "has_attention",
-            "pse_data",
-            "has_inception",
-            "is_previous",
-            "is_normal",
-            "has_dropout",
-            "is_enn",
-            "is_mul_layer",
-            "has_nrem2_bias",
-            "has_rem_bias",
-            "dropout_rate",
-            "batch_size",
-            "n_class",
-            "kernel_size",
-            "stride",
-            "sample_size",
-        ]
-
-        # 辞書の初期値のみをループとは別に作成しておく
-        kernel_d = {_params: [] for _params in params}
-        list4loop = [dataset]
-        for _list4loop in list4loop:
-            merged_d = {__list4loop: kernel_d for __list4loop in _list4loop}
-            kernel_d = merged_d.copy()
-        # jsonの中身の書き換え
-        self.json_dict = merged_d
-        # jsonfileへの書き込み
-        try:
-            with open(self.json_file, "w") as f:
-                json.dump(self.json_dict, f, indent=2)
-        except Exception:
-            print("jsonへの書き込みに失敗しました")
-            sys.exit(1)
+        self.make_deep_dict(list4loop, cleansing_type_d)
 
     # フォーマット作成のメタメソッド
     def make_format(self, json: str) -> None:
@@ -365,13 +323,30 @@ class JsonBase(object):
             self.make_model_id_format()
         elif json == "subjects_list":
             # self.make_subjects_list_format()
-            print("実装まだです．飛ばします")
+            print(
+                PyColor().RED_FLASH,
+                f"{json} is not implemented. Skipping .. ",
+                PyColor().END,
+            )
             return
         elif json == "subjects_info":
             self.make_subjects_info_format()
         else:
-            print("実装まだです．飛ばします")
+            print(
+                PyColor().RED_FLASH,
+                f"{json} is not implemented. Skipping .. ",
+                PyColor().END,
+            )
             return
+
+    # host名の第一キーを返す
+    def get_hostkey(self) -> str:
+        if self.hostname == "DESKTOP-0CSCRP7":
+            return "home-pc"
+        elif self.hostname == "spica-2nd":
+            return self.hostname
+        elif self.hostname == "DESKTOP-TPBSQ67":
+            return "win_249"
 
     # pre_processの第一キーを返す
     def first_key_of_pre_process(self, is_normal: bool, is_prev: bool) -> str:
@@ -390,12 +365,12 @@ class JsonBase(object):
 
 if __name__ == "__main__":
     filenames = [
-        # "pre_processed_id.json",
-        # "model_id.json",
+        "pre_processed_id.json",
+        "model_id.json",
         # "my_color.json",
-        # "ss.json",
-        # "subjects_list.json",
-        "subjects_info.json"
+        "ss.json",
+        "subjects_list.json",
+        "subjects_info.json",
     ]
     for _filenames in filenames:
         jb = JsonBase(_filenames)
