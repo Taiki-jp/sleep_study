f38f09e546223bba8707945250a4f14ad035babf
taiki-jp <taiki-dosisha@outlook.jp>
2022-01-03 10:51:19 +0900

    fix: my_color.py

--- data_analysis/my_color.py
+++ data_analysis/my_color.py
@@ -4,18 +4,17 @@ from pre_process.json_base import JsonBase
 class MyColor(JsonBase):
     def __init__(self) -> None:
         super().__init__(json_filename="my_color.json")
+        self.color = self.json_dict["color"]
+        print(self.color)
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    import numpy as np
-
     my_color = MyColor()
-    my_color.load()
-    for counter, (alias, color) in enumerate(my_color.json_dict.items()):
-        if not callable(color):
-            x = np.arange(10)
-            y = x * (counter + 1)
-            plt.plot(x, y, c=color, label=f"alias : {alias}")
-    plt.legend()
-    plt.show()
+    # my_color.load()
+    # for counter, (alias, color) in enumerate(my_color.json_dict.items()):
+    #     if not callable(color):
+    #         x = np.arange(10)
+    #         y = x * (counter + 1)
+    #         plt.plot(x, y, c=color, label=f"alias : {alias}")
+    # plt.legend()
+    # plt.show()
