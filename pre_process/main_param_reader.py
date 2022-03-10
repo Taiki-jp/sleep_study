5f708c0f9b1e55f22bdbb4917a3e8638a8a9b74b
taiki-jp <taiki-dosisha@outlook.jp>
2022-03-11 05:10:22 +0900

    feat: new denn_ensemble member

--- pre_process/main_param_reader.py
+++ pre_process/main_param_reader.py
@@ -5,6 +5,7 @@ class MainParamReader(JsonBase):
     def __init__(self, json_filename: str = "main_param.json") -> None:
         super().__init__(json_filename)
         self.main_setting = self.json_dict["main"]
+        self.denn_ensemble = self.json_dict["denn_ensemble"]
 
 
 if __name__ == "__main__":
