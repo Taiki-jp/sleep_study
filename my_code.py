654b36da19db86e97b71cebb73cf6f527ddc9eb3
taiki-jp <taiki-dosisha@outlook.jp>
2022-03-22 03:30:13 +0900

    :construction: Delete unused files
    使ってなさそうなファイルを削除 or 移動．結構あいまいなのでマージの際は注意(NOTE)

---
+++ my_code.py
@@ -0,0 +1,23 @@
+# 2クラス分類の結果をマージする
+from data_analysis.utils import Mine
+
+
+def make_ss_graph():
+    VERBOSE = 1
+    # 提案手法（キー）とそのパスのための情報（バリュー）の辞書
+    path_arg_d = {
+        "cnn_attn": ["cnn_attn_5stage", "*.csv"],
+        "cnn_no_attn": ["cnn_noattn_5stage", "*.csv"],
+        "aecnn": ["aecnn", "*.csv"],
+        "ecnn": ["ecnn", "*.csv"],
+        "ccnn_ver1": ["ccnn", "*.csv"],
+        "eenn": ["eenn", "*.csv"],
+        "denn": ["denn", "*.csv"],
+    }
+    mine = Mine(path_arg_d, VERBOSE)
+    mine.exec()
+    return
+
+
+if __name__ == "__main__":
+    make_ss_graph()
